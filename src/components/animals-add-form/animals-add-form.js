f27f5b1bb204fb24e4891ae989eebf384583cad3
Yura Dubinin <yuradubinin100@gmail.com>
2022-03-10 14:24:48 +0700

    Working with the 'animals-add-form' component
    * Added method onValueChange for both inputs
    
    * 'animals-add-form' is managed component

--- src/components/animals-add-form/animals-add-form.js
+++ src/components/animals-add-form/animals-add-form.js
@@ -1,19 +1,53 @@
+import { Component } from "react";
+
 import "./animals-add-form.css";
 
-const AnimalsAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавить нового</h3>
-            <form className="add-form d-flex">
-                <input type="text" className="form-control new-post-label" placeholder="Имя" />
-                <input type="number" className="form-control new-post-label" placeholder="Вид" />
+class AnimalsAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            kindOfAnimal: "",
+        };
+    }
 
-                <button type="submit" className="btn btn-outline-light">
-                    Добавить
-                </button>
-            </form>
-        </div>
-    );
-};
+    onValueChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    render() {
+        const { name, kindOfAnimal } = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавить нового</h3>
+                <form className="add-form d-flex">
+                    <input
+                        type="text"
+                        className="form-control new-post-label"
+                        placeholder="Имя"
+                        name="name"
+                        value={name}
+                        onChange={this.onValueChange}
+                    />
+                    <input
+                        type="text"
+                        className="form-control new-post-label"
+                        placeholder="Вид"
+                        name="kindOfAnimal"
+                        value={kindOfAnimal}
+                        onChange={this.onValueChange}
+                    />
+
+                    <button type="submit" className="btn btn-outline-light">
+                        Добавить
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default AnimalsAddForm;
