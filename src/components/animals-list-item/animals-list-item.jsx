5a815c4c0a3cbe3dc7ecbb8887c3c4b2eb3958f9
Yura Dubinin <yuradubinin100@gmail.com>
2022-04-15 01:46:23 +0700

    AnimalListItem component improved
    Added styleDistributor method for the distribution of style classes for indicators
    
    * Added special selector in animals-list-item.scss
    
    * Added special selectors in variables.scss

--- src/components/animals-list-item/animals-list-item.jsx
+++ src/components/animals-list-item/animals-list-item.jsx
@@ -1,6 +1,12 @@
 import "./animals-list-item.scss";
 
 const AnimalsListItem = ({ name, kindOfAnimal, treatment, specialAttention, inDangerousState }) => {
+    //Adding a passivity class to indicators depending on a property
+    const styleDistributor = (property) => {
+        const className = property ? null : "passive-health-indicator";
+        return className;
+    };
+
     return (
         <li className="animals-list-item">
             <div className="">
@@ -21,14 +27,14 @@ const AnimalsListItem = ({ name, kindOfAnimal, treatment, specialAttention, inDa
                 </span>
             </div>
 
-            <ul className="animals-list-item__indicators">
-                <li>
+            <ul className="animals-list-item__health-indicators">
+                <li className={styleDistributor(treatment)}>
                     <i className="fa-solid fa-eye" title="Дополнительное наблюдение"></i>
                 </li>
-                <li>
+                <li className={styleDistributor(specialAttention)}>
                     <i className="fa-solid fa-comment-medical" title="Находится на лечении"></i>
                 </li>
-                <li>
+                <li className={styleDistributor(inDangerousState)}>
                     <i className="fa-solid fa-bolt" title="В опасном состоянии"></i>
                 </li>
             </ul>
