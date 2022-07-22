0fc6b0a153feab9dc2a0925a52f3a62ccfedec18
Yura Dubinin <yuradubinin100@gmail.com>
2022-07-22 12:47:46 +0300

    Preparing for a massive refactoring pt.1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/App";
 
-import "./style/style.scss";
+import "./styles/style.scss";
 
 ReactDOM.render(
     <React.StrictMode>
